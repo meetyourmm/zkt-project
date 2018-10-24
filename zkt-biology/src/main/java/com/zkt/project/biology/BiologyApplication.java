9cbd8871e67cac70e6c32c03cc508a452eac2ad0
LiWei <liwei@ibeifeng.com>
2018-10-24 13:07:04 +0800

    zkt-biology

---
+++ zkt-biology/src/main/java/com/zkt/project/biology/BiologyApplication.java
@@ -0,0 +1,22 @@
+package com.zkt.project.biology;
+
+import com.zkt.common.web.EnableSoaClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@EnableSwagger2
+@MapperScan(basePackages ={"com.zkt.project.admin.mapper"} )
+@ComponentScan(basePackages = {"com.zkt.project.biology","com.zkt.common.web"})
+@EnableSoaClient
+@EnableCaching
+public class BiologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiologyApplication.class, args);
+	}
+}

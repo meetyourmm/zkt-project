67f4ec0d3f2d9b31b32b3d4db569654b7e64073e
LiWei <liwei@ibeifeng.com>
2019-01-15 16:30:00 +0800

    跨域设置

--- zkt-admin/src/main/java/com/zkt/project/admin/AdminApplication.java
+++ zkt-admin/src/main/java/com/zkt/project/admin/AdminApplication.java
@@ -3,8 +3,15 @@ package com.zkt.project.admin;
 import com.zkt.common.web.EnableSoaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -14,9 +21,30 @@ import tk.mybatis.spring.annotation.MapperScan;
 @ComponentScan(basePackages = {"com.zkt.project.admin","com.zkt.common.web"})
 @EnableSoaClient
 @EnableCaching
-public class AdminApplication {
+public class AdminApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AdminApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		// 设置你要允许的网站域名，如果全允许则设为 *
+		config.addAllowedOrigin("*");
+		// 如果要限制 HEADER 或 METHOD 请自行更改
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		// 这个顺序很重要哦，为避免麻烦请设置在最前
+		bean.setOrder(0);
+		return bean;
 	}
 }
